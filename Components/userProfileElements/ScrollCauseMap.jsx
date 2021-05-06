030aaa854ac4b78a889ea9dad6166c3ce6f0d575
Richard Banh <rbanh@me.com>
2021-05-06 14:42:36 -0700

    figured out the colouring of the UI

--- Components/userProfileElements/ScrollCauseMap.jsx
+++ Components/userProfileElements/ScrollCauseMap.jsx
@@ -1,23 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { set } from "react-native-reanimated";
 
-import { CAUSE } from "../../data/dumby.js";
+import { CAUSE, SELECTEDCAUSE } from "../../data/dumby.js";
 
 export const ScrollCause = () => {
 	const [causeData, setCause] = useState([]);
+	const [selected, setSelected] = useState([]);
+	const [loaded, setLoad] = useState(false);
+	const [unselected, setUnselected] = useState([]);
+
+	function subobj(arr1, arr2) {
+		const result = arr1.filter((ad) =>
+			arr2.every((fd) => fd.cause !== ad.cause)
+		);
+		return result;
+	}
+
 	useEffect(() => {
-		setCause(CAUSE);
+		// setCause(CAUSE);
+		setSelected(SELECTEDCAUSE);
+		setUnselected(subobj(CAUSE, SELECTEDCAUSE));
+		setLoad(true);
 	}, []);
 
 	return (
 		<View style={{ maxheight: 140 }}>
 			<ScrollView style={{ width: "100%" }}>
 				<View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-					{causeData.map((x) => (
-						<View style={styles.bubble}>
-							<Text>{x.cause}</Text>
-						</View>
-					))}
+					{loaded ? (
+						selected.map(
+							(x) => (
+								<View style={styles.bubble}>
+									<Text>{x.cause}</Text>
+								</View>
+							),
+							unselected.map((x) => (
+								<View style={styles.bubble}>
+									<Text>{x.cause}</Text>
+								</View>
+							))
+						)
+					) : (
+						<Text></Text>
+					)}
 				</View>
 			</ScrollView>
 		</View>
